9ffab236a2814ddb9f03f2f0207457a3e0ddb07f
Dilsha Moideen <dilmoidi@gmail.com>
2026-06-10 12:09:59 +0530

    Fix Render seeding

--- build.sh
+++ build.sh
@@ -12,10 +12,7 @@ echo "Running migrations..."
 python manage.py migrate
 
 echo "Seeding data..."
-# Run custom populate scripts to seed initial database info (movies and foods)
-python manage.py shell -c "
-from api.models import City
-if not City.objects.exists():
-    import populate_movies
-    import populate_food
-"
+
+python populate_movies.py
+python populate_food.py
+
